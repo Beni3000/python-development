66b43c5acf7891f3339d9a830803754845178ef8
beni3000 <beni-3000@yandex.ru>
2024-12-10 21:12:30 +0300

    lesson_10_12

--- scripts/currency.py
+++ scripts/currency.py
@@ -1,10 +1,31 @@
-import reguests
-for bs4 inport BeautifulSoup as Bs
+import requests
+
+from bs4 import BeautifulSoup as Bs
+from requests.exceptions import ConnectionError
 
 
-def get_html(url: str) => str:
-    response = reguests.get(url)
+def get_html(url: str) -> str|None:
+    try:
+        response = requests.get(url)
+        status = response.status_code
+        if status != 200 and str(status)[0] !=3:
+            print(f'ошибка запроса. Код ответа - {status}')
+            return None
+        print(f"Код ответа - {status}")
+        html = response.text
+        return html
+    except ConnectionError as error:
+        print('Нет ответа от сервера')
+        print(type(error))
+        return None
+
+def parse_html(html: str):
+    soup: Bs = Bs(html, 'html.parser')
+    current_data = soup.find('h2', class_="h2 blue").text.split('\n')[-1].strip()[:10]
+    # table =
     pass
 
-URL = "https://www.alta.ru/currency/"
-get_html(URL)
+URL = "https://www.alta.ru/currency"
+html = get_html(URL)
+if html:
+    parse_html(html)
