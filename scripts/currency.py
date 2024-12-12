038b3ed9ec218495f6a2070cce756eb41b03e761
Beni3000 <beni-3000@yande.ru>
2024-12-12 15:27:13 +0300

    homework_12_12

--- scripts/currency.py
+++ scripts/currency.py
@@ -1,15 +1,17 @@
 import requests
+import json
 
 from bs4 import BeautifulSoup as Bs
 from requests.exceptions import ConnectionError
 
 
-def get_html(url: str) -> str|None:
+
+def get_html(url: str) -> str | None:
     try:
         response = requests.get(url)
         status = response.status_code
         if status != 200 and str(status)[0] !=3:
-            print(f'ошибка запроса. Код ответа - {status}')
+            print(f'Ошибка запроса. Код ответа - {status}')
             return None
         print(f"Код ответа - {status}")
         html = response.text
@@ -19,13 +21,46 @@ def get_html(url: str) -> str|None:
         print(type(error))
         return None
 
-def parse_html(html: str):
+def parse_html(html: str) -> dict:
+    courses = {}
     soup: Bs = Bs(html, 'html.parser')
     current_data = soup.find('h2', class_="h2 blue").text.split('\n')[-1].strip()[:10]
-    # table =
-    pass
+    table = soup.find('div', class_="module-tableSort")
+    rows = table.find_all('tr')[2:]
+    courses[current_data] = {}
 
-URL = "https://www.alta.ru/currency"
-html = get_html(URL)
-if html:
-    parse_html(html)
+    for row in rows:
+        if not row.find('td', class_='t-center'):
+            continue
+        code = row.find('td', class_='t-center').text
+        number_code = code.split()[0]
+        txt_code = code.split()[1]
+
+        name = row.find('td', class_='t-left').text.split('\n')[0]
+        value = row.find('td', class_='t-right').text.split()
+        print(f'{number_code}-{txt_code} {name} {value}')
+        courses[current_data][txt_code] = {
+            "number_code": number_code,
+            "name": name,
+            "value": value
+        }
+    return courses
+
+def write_data_to_json(data: dict) -> None:
+    with open('courses_json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+def get_data_from_json(file_path: str) -> dict:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+# URL = "https://www.alta.ru/currency"
+# html = get_html(URL)
+# if html:
+#     courses = parse_html(html)
+#     write_data_to_json(data=courses)
+
+
+courses_from_file = get_data_from_json('courses_json')
+print(courses_from_file)
